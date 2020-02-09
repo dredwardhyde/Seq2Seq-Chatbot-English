fd973271ee6cde6dcc1bd14df26ea616a2d272db
edwardhyde <komesergey@gmail.com>
2020-02-09 10:31:53 +0300

    Added bigger chatbot

---
+++ chatbot_big.py
@@ -0,0 +1,186 @@
+import codecs
+import requests, zipfile, io
+import numpy as np
+import tensorflow as tf
+from gensim.models import Word2Vec
+import re
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+r = requests.get('http://www.cs.cornell.edu/~cristian/data/cornell_movie_dialogs_corpus.zip')
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall()
+
+conversations = []
+with codecs.open("./cornell movie-dialogs corpus/movie_lines.txt", "rb", encoding="utf-8", errors="ignore") as f:
+    lines = f.read().split("\n")
+    for line in lines:
+        data = line.split(" +++$+++ ")
+        conversations.append(data)
+print("Total conversations in dataset: {}".format(len(conversations)))
+
+chats = {}
+for tokens in conversations[:10000]:
+    if len(tokens) > 4:
+        idx = tokens[0][1:]
+        chat = tokens[4]
+        chats[int(idx)] = chat
+
+sorted_chats = sorted(chats.items(), key = lambda x: x[0])
+
+conves_dict = {}
+counter = 1
+conves_ids = []
+for i in range(1, len(sorted_chats)+1):
+    if i < len(sorted_chats):
+        if (sorted_chats[i][0] - sorted_chats[i-1][0]) == 1:
+            if sorted_chats[i-1][1] not in conves_ids:
+                conves_ids.append(sorted_chats[i-1][1])
+            conves_ids.append(sorted_chats[i][1])
+        elif (sorted_chats[i][0] - sorted_chats[i-1][0]) > 1:
+            conves_dict[counter] = conves_ids
+            conves_ids = []
+        counter += 1
+    else:
+        pass
+
+context_and_target = []
+for conves in conves_dict.values():
+    if len(conves) % 2 != 0:
+        conves = conves[:-1]
+    for i in range(0, len(conves), 2):
+        context_and_target.append((conves[i], conves[i+1]))
+context, target = zip(*context_and_target)
+questions = list(context)
+target_dirty = list(target)
+answers = list()
+for i in range(len(target_dirty)):
+    answers.append('<START> ' + target_dirty[i] + ' <END>')
+
+print(len(questions))
+print(len(answers))
+
+tokenizer = tf.keras.preprocessing.text.Tokenizer(filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~\t\n\'0123456789')
+tokenizer.fit_on_texts(questions + answers)
+VOCAB_SIZE = len(tokenizer.word_index) + 1
+
+print('VOCAB SIZE : {}'.format(VOCAB_SIZE))
+
+vocab = []
+for word in tokenizer.word_index:
+    vocab.append(word)
+
+
+def tokenize(sentences):
+    tokens_list = []
+    vocabulary = []
+    for sentence in sentences:
+        sentence = sentence.lower()
+        sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+        tokens = sentence.split()
+        vocabulary += tokens
+        tokens_list.append(tokens)
+    return tokens_list, vocabulary
+
+
+p = tokenize(questions + answers)
+model = Word2Vec(p[0], min_count=1)
+
+embedding_matrix = np.zeros((VOCAB_SIZE, 100))
+for i in range(len(tokenizer.word_index)):
+    embedding_matrix[i] = model[vocab[i]]
+
+tokenized_questions = tokenizer.texts_to_sequences(questions)
+maxlen_questions = max([len(x) for x in tokenized_questions])
+padded_questions = tf.keras.preprocessing.sequence.pad_sequences(tokenized_questions, maxlen=maxlen_questions, padding='post')
+encoder_input_data = np.array(padded_questions)
+
+print(encoder_input_data.shape, maxlen_questions)
+
+tokenized_answers = tokenizer.texts_to_sequences(answers)
+maxlen_answers = max([len(x) for x in tokenized_answers])
+padded_answers = tf.keras.preprocessing.sequence.pad_sequences(tokenized_answers, maxlen=maxlen_answers, padding='post')
+decoder_input_data = np.array(padded_answers)
+
+print(decoder_input_data.shape, maxlen_answers)
+
+tokenized_answers = tokenizer.texts_to_sequences(answers)
+for i in range(len(tokenized_answers)):
+    tokenized_answers[i] = tokenized_answers[i][1:]
+padded_answers = tf.keras.preprocessing.sequence.pad_sequences(tokenized_answers, maxlen=maxlen_answers, padding='post')
+onehot_answers = tf.keras.utils.to_categorical(padded_answers, VOCAB_SIZE)
+decoder_output_data = np.array(onehot_answers)
+
+print(decoder_output_data.shape)
+
+encoder_inputs = tf.keras.layers.Input(shape=(None,))
+encoder_embedding = tf.keras.layers.Embedding(VOCAB_SIZE, 200, mask_zero=True)(encoder_inputs)
+encoder_outputs, state_h, state_c = tf.keras.layers.LSTM(200, return_state=True)(encoder_embedding)
+encoder_states = [state_h, state_c]
+
+decoder_inputs = tf.keras.layers.Input(shape=(None,))
+decoder_embedding = tf.keras.layers.Embedding(VOCAB_SIZE, 200, mask_zero=True)(decoder_inputs)
+decoder_lstm = tf.keras.layers.LSTM(200, return_state=True, return_sequences=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_embedding, initial_state=encoder_states)
+decoder_dense = tf.keras.layers.Dense(VOCAB_SIZE, activation=tf.keras.activations.softmax)
+output = decoder_dense(decoder_outputs)
+
+model = tf.keras.models.Model([encoder_inputs, decoder_inputs], output)
+model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss='categorical_crossentropy')
+
+model.summary()
+
+model.fit([encoder_input_data, decoder_input_data], decoder_output_data, batch_size=50, epochs=300)
+model.save('model_big.h5')
+#model.load_weights('model_big.h5')
+
+
+def make_inference_models():
+    encoder_model = tf.keras.models.Model(encoder_inputs, encoder_states)
+    decoder_state_input_h = tf.keras.layers.Input(shape=(200,))
+    decoder_state_input_c = tf.keras.layers.Input(shape=(200,))
+    decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+    decoder_outputs, state_h, state_c = decoder_lstm(decoder_embedding, initial_state=decoder_states_inputs)
+    decoder_states = [state_h, state_c]
+    decoder_outputs = decoder_dense(decoder_outputs)
+    decoder_model = tf.keras.models.Model(
+        [decoder_inputs] + decoder_states_inputs,
+        [decoder_outputs] + decoder_states)
+    return encoder_model, decoder_model
+
+
+def str_to_tokens(sentence: str):
+    words = sentence.lower().split()
+    tokens_list = list()
+    for word in words:
+        tokens_list.append(tokenizer.word_index[word])
+    return tf.keras.preprocessing.sequence.pad_sequences([tokens_list], maxlen=maxlen_questions, padding='post')
+
+
+enc_model, dec_model = make_inference_models()
+
+for _ in range(100):
+    states_values = enc_model.predict(str_to_tokens(input('Enter question : ')))
+    empty_target_seq = np.zeros((1, 1))
+    empty_target_seq[0, 0] = tokenizer.word_index['start']
+    stop_condition = False
+    decoded_translation = ''
+    while not stop_condition:
+        dec_outputs, h, c = dec_model.predict([empty_target_seq] + states_values)
+        sampled_word_index = np.argmax(dec_outputs[0, -1, :])
+        sampled_word = None
+        for word, index in tokenizer.word_index.items():
+            if sampled_word_index == index:
+                decoded_translation += ' {}'.format(word)
+                sampled_word = word
+
+        if sampled_word == 'end' or len(decoded_translation.split()) > maxlen_answers:
+            stop_condition = True
+
+        empty_target_seq = np.zeros((1, 1))
+        empty_target_seq[0, 0] = sampled_word_index
+        states_values = [h, c]
+
+    print(decoded_translation)
+
